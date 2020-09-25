b24fe6569540e6dd540c5a1ad8a9b267f1392af8
Sandeep0045 <38896678+Sandeep0045@users.noreply.github.com>
2020-09-25 09:30:37 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,6 +1,6 @@
 variable "okta_group_name"{}
 
-variable "okta_group_description" {}
+variable "okta_group_description"{}
 
 variable "okta_api_token" {
   default = "00WDvVIr3Y6Wg3o08fG_Pxstf6qNIGgcKQEOVHw5sy"
