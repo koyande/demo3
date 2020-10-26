2792714dc1bc27bda6b49f013eb8f6e371b8f721
Sandeep0045 <38896678+Sandeep0045@users.noreply.github.com>
2020-10-26 19:10:59 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -6,5 +6,5 @@ variable "okta_group_description"{
   default = "this is my map group"
   }
 variable "okta_api_token" {
-  default = "00WDvVIr3Y6Wg3o08fG_Pxstf6qNIGgcKQEOVHw5sy"
+  default =  "00oN74cr8Nl72ci3zu16IGVSn_cMPENQDr3y0y-Kf5"
 }
