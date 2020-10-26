57ab016496ad413e65d4e38b21fc38a5c50e5e2d
Sandeep0045 <38896678+Sandeep0045@users.noreply.github.com>
2020-10-26 19:31:30 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,10 +1,11 @@
 variable "okta_group_name"{
-  default = "hap"
+  default = ""
   }
 
 variable "okta_group_description"{
-  default = "this is hap group"
+  default = ""
   }
+
 variable "okta_api_token" {
   default =  "00oN74cr8Nl72ci3zu16IGVSn_cMPENQDr3y0y-Kf5"
 }
